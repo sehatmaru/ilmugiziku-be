d23c8e7cc6b6f099e74fbdebc037849253c54c73
sehatmaru10 <sehat.maru@gmail.com>
2020-03-26 00:29:15 +0700

    Initial Commit

---
+++ src/main/java/com/homestay/be/HomestayBeApplication.java
@@ -0,0 +1,13 @@
+package com.homestay.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomestayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomestayBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homestay/be/HomestayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homestay.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
