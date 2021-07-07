1491e04fb7064c7af0af5784501b1b26ae7c69e4
Sehat Maruli Tua Samosir <maruzhaky@gmail.com>
2021-07-07 15:12:38 +0700

    revert mainapp

--- src/main/java/xcode/ilmugiziku/MainApplication.java
+++ src/main/java/xcode/ilmugiziku/MainApplication.java
@@ -20,7 +20,7 @@ public class MainApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
 			}
 		};
 	}
