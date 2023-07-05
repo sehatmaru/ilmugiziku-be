ca2e71052f23cf4a926b5a397de601aad6f46bc3
sehatmaru <maruzhaky@gmail.com>
2023-07-05 14:04:31 +0700

    callback token

--- src/main/java/xcode/ilmugiziku/shared/Environment.java
+++
@@ -1,7 +0,0 @@
-package xcode.ilmugiziku.shared;
-
-public class Environment {
-    public static final int MAX_TOKEN_DAYS = 1;
-    public static final String XENDIT_TOKEN = "49R3MxxTC1Pe1QDxI2EDqRLcySDHhsD3j6ChwmDUHHYxdtjM";
-    public static final String XENDIT_API = "xnd_development_KYHjDJS2RCYQhCJYeDAJVOGkGyz0DG9RI5EaxFpMipRrxi2peUqueLf9j4AEVsP2";
-}
